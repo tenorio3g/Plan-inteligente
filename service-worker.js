24bddb47908a28e28b8aadf7e2533d7d3b51d9be
Fortunato Tenorio <tenorio.3g@hotmail.com>
2025-08-02 08:29:48 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,18 @@
-
-const CACHE_NAME = 'form-cache-v2';
-const FILES_TO_CACHE = [
-  './',
-  './index.html',
-  './style.css',
-  './manifest.json',
-  './icon.png'
-];
-
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open('pwa-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon.png'
+      ]);
+    })
   );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      })
-    ))
-  );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
